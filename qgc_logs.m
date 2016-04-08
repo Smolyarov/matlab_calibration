2992fcfb51e255a1c45cd0f6edd2c18fed35c56b
Smolyarov <smolyarov@ntlab.com>
2016-04-08 17:30:04 +0300

    Got polynomial coeffs for accelerometer

--- qgc_logs.m
+++ qgc_logs.m
@@ -2,25 +2,22 @@ close all
 clear all
 clc
 
-logfile_path = '/home/pilgrim/QGC_logs/';
 num_logfiles = 6;
-row_start = 7;
 B_mag = 500;
-B_acc = -9810;
+B_acc = 9810;
 B_gyr = 0;
 temp_base = 30; % celsius
-temp_hi = 48;
-temp_lo = 44;
+temp_lo = 36;
+temp_hi = 61;
 
 cnt = 0;
 for i = 1:num_logfiles
-    logfile = strcat(logfile_path,int2str(i),'_compressed.txt');
-    disp(logfile);
-    log_data = dlmread(logfile,'\t',row_start,1);
-    log_data(:,4) = round(log_data(:,4)); % round temperature
+    load(strcat(int2str(i),'.mat'));
+    disp(strcat(int2str(i),'.mat'));
+    log_data = [double(acc) round(temp)];
     
-    for temp = temp_lo:temp_hi
-        log_part = log_data(log_data(:,4)==temp,:); % measurements with equal temperature
+    for t = temp_lo:temp_hi
+        log_part = log_data(log_data(:,4)==t,:); % measurements with equal temperature
         if (~isempty(log_part))
             cnt = cnt + 1;
             meas_avg = mean(log_part,1);
@@ -41,19 +38,19 @@ end
 pointsCount = num_logfiles;
 
 cnt = 0;
-for temp = temp_lo:temp_hi
+for t = temp_lo:temp_hi
     imuCalibrator = Calibrator('vectorModule', B_acc, ...
                            'calibrationParametersCount', 6, ...
                            'type', 'recursive', ...
                            'R', 5, ...
                            'P', 1e30);
-    calib_in = (prep_data(prep_data(:,4)==temp,1:3))';
+    calib_in = (prep_data(prep_data(:,4)==t,1:3))';
     calib_out = zeros(3,pointsCount);
     input_mod = zeros(1, pointsCount);
     output_mod = zeros(1, pointsCount);
     if (~isempty(calib_in))
-        temp
-        calib_in
+%         t
+%         calib_in
         for i = 1 : pointsCount
         imuCalibrator.calibration(calib_in(:,i));
         end
@@ -62,30 +59,15 @@ for temp = temp_lo:temp_hi
         input_mod(1,i) = norm(calib_in(:, i));
         output_mod(1,i) = norm(calib_out(:, i));
         end   
-%         imuCalibrator.bias
-%         imuCalibrator.Winv
         cnt = cnt + 1;
         WINV(cnt,:) = diag(imuCalibrator.Winv);
         BIAS(cnt,:) = imuCalibrator.bias;
-        
-        
-%         figure
-%         plot3(calib_in(1,:),calib_in(2,:),calib_in(3,:),'r.');
-%         hold on;
-%         plot3(calib_out(1,:),calib_out(2,:),calib_out(3,:),'.');
-%         axis equal
-%         grid on
-% 
-%         figure;
-%         plot(input_mod,'r');
-%         hold on;
-%         plot(output_mod,'b');
     end
 end
-
-
-
-
-
-
-% debug 43 - temp C
+temp = (temp_lo:temp_hi)';
+for i = 1:3
+    Pbias(i,:) = polyfit(temp,BIAS(:,i),3);
+    Pscale(i,:) = polyfit(temp,WINV(:,i),3);
+end
+Pbias
+Pscale
